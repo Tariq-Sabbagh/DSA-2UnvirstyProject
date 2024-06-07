157da13bea1f0d8eab6daae996e543631cf8c270
Tariq <teto75928@gmail.com>
2024-06-07 06:16:00 -0700

    A first version of the project First issue

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,5 @@
-import java.util.*;
-
-
-
 public class Main {
     public static void main(String[] args) {
-        GeneralTree GT=new GeneralTree();
-        BinaryTree BT=new BinaryTree();
-        GT.CreateTree();
-        String treeString = GT.TreeToString(GT.root);
-        GT.Import(treeString);
-        System.out.println("Level order traversal ");
-        GT.printGeneralTree(GT.root ,0);
-        GT.Export(GT.ConvertFileToList());
-
-
-
-        BinaryTreeNode binaryTreeRoot = BT.ConvertGeneralToBinary(GT.root);
-        BT.printBinaryTree(binaryTreeRoot, "");
-
-           Node generalTreeRoot = BT.ConvertBinaryToGeneral(binaryTreeRoot);
-          GT.printGeneralTree(generalTreeRoot, 0);
-
 
     }
 }
